6e183d43f3e6975597e5ef3e0a2ee97abbb72913
Katie Berkowitz <katieb@Katies-MacBook-Pro.local>
2021-07-30 10:31:05 -0400

    edits

---
+++ Katie_Working/app2.js
@@ -0,0 +1,116 @@
+
+// Trace1 
+  var trace1 = {
+    x: edu_data.map(row => row.year),
+    y: edu_data.map(row => row.education),
+    type: "line",
+    name: "Education"
+  };
+
+// Trace 2 
+  var trace2 = {
+    x: edu_data.map(row => row.year),
+    y: edu_data.map(row => row.military),
+    type: "line",
+    name: "Military"
+  };
+  var trace3 = {
+    x: edu_data.map(row => row.year),
+    y: edu_data.map(row => row.employment),
+    type: "line",
+    name: "Employment"
+  };
+
+  var trace4 = {
+    x: edu_data.map(row => row.year),
+    y: edu_data.map(row => row.other),
+    type: "line",
+    name: "Other"
+  };
+
+
+// Combining both traces
+  var traceData = [trace1, trace2, trace3, trace4];
+
+  var layout = {
+    title: "Post-Secondary Plans for the Commonwealth of Virginia",
+    
+  };
+
+  Plotly.newPlot("line-next", traceData, layout)
+//}
+
+// Trace1 
+var traceVA = {
+  x: nat_avg.map(row => row.Year),
+  y: nat_avg.map(row => row.virginia_avg),
+  type: "line",
+  name: "Virginia Average"
+};
+
+// Trace 2 
+var traceNAT = {
+  x: nat_avg.map(row => row.Year),
+  y: nat_avg.map(row => row.nat_avg),
+  type: "line",
+  name: "National Average"
+};
+var traceHigh = {
+  x: nat_avg.map(row => row.Year),
+  y: nat_avg.map(row => row.high_pay),
+  type: "line",
+  name: "Highest State Average"
+};
+
+var traceLow = {
+  x: nat_avg.map(row => row.Year),
+  y: nat_avg.map(row => row.low_pay),
+  type: "line",
+  name: "Lowest State Average"
+};
+
+
+// Combining both traces
+var traceData2 = [traceVA, traceNAT, traceHigh,traceLow];
+
+var layout2 = {
+  title: "National Teacher Salary Comparison",
+  
+};
+
+Plotly.newPlot("line-avg", traceData2, layout2)
+
+var traceTeach = {
+  x: teacher_pay.map(row => row.Year),
+  y: teacher_pay.map(row => row.avg_teach),
+  type: "line",
+  name: "Virginia Teacher Average Salary"
+};
+
+// Trace 2 
+var tracePrinc = {
+  x: teacher_pay.map(row => row.Year),
+  y: teacher_pay.map(row => row.avg_princ),
+  type: "line",
+  name: "Virginia Principle Average Salary"
+};
+var traceAsst = {
+  x: teacher_pay.map(row => row.Year),
+  y: teacher_pay.map(row => row.avg_asst),
+  type: "line",
+  name: "Virginia Assistant Principle Average Salary"
+};
+
+
+
+
+// Combining both traces
+var traceData3 = [traceTeach, tracePrinc, traceAsst];
+
+var layout3 = {
+  title: "Virginia Salary Comparison",
+  
+};
+
+Plotly.newPlot("line-comp", traceData3, layout3)
+
---
+++ Katie_Working/data.js
@@ -0,0 +1,128 @@
+var edu_data = [
+  {
+    "year": 2003,
+    "education": 56725,
+    "employment": 7557,
+    "military": 2273,
+    "other": 8380
+  },
+  {
+    "year": 2004,
+    "education": 56543,
+    "employment": 9449,
+    "military": 2334,
+    "other": 6775
+  },
+  {
+    "year": 2005,
+    "education": 61078,
+    "employment": 9461,
+    "military": 2093,
+    "other": 4210
+  },
+  {
+    "year": 2006,
+    "education": 61420,
+    "employment": 9365,
+    "military": 2206,
+    "other": 4571
+  },
+  {
+    "year": 2007,
+    "education": 66523,
+    "employment": 9556,
+    "military": 2140,
+    "other": 3773
+  },
+  {
+    "year": 2008,
+    "education": 69014,
+    "employment": 10027,
+    "military": 2509,
+    "other": 4343
+  },
+  {
+    "year": 2009,
+    "education": 70574,
+    "employment": 9511,
+    "military": 3037,
+    "other": 4595
+  },
+  {
+    "year": 2010,
+    "education": 73297,
+    "employment": 9075,
+    "military": 3260,
+    "other": 3522
+  },
+  {
+    "year": 2011,
+    "education": 73443,
+    "employment": 9401,
+    "military": 3597,
+    "other": 3369
+  },
+  {
+    "year": 2012,
+    "education": 73562,
+    "employment": 9596,
+    "military": 3692,
+    "other": 3405
+  },
+  {
+    "year": 2013,
+    "education": 73938,
+    "employment": 8997,
+    "military": 3819,
+    "other": 2749
+  },
+  {
+    "year": 2014,
+    "education": 72847,
+    "employment": 8914,
+    "military": 3755,
+    "other": 2617
+  },
+  {
+    "year": 2015,
+    "education": 72115,
+    "employment": 8717,
+    "military": 3655,
+    "other": 2368
+  },
+  {
+    "year": 2016,
+    "education": 74147,
+    "employment": 9594,
+    "military": 3711,
+    "other": 2739
+  },
+  {
+    "year": 2017,
+    "education": 73682,
+    "employment": 10188,
+    "military": 3499,
+    "other": 2429
+  },
+  {
+    "year": 2018,
+    "education": 75962,
+    "employment": 10570,
+    "military": 3577,
+    "other": 2512
+  },
+  {
+    "year": 2019,
+    "education": 74394,
+    "employment": 10994,
+    "military": 3608,
+    "other": 3763
+  },
+  {
+    "year": 2020,
+    "education": 72484,
+    "employment": 11870,
+    "military": 3171,
+    "other": 5975
+  }
+];
